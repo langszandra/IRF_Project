7117dc7eef14ffd980cd2e6ad90b3e3342fea5ae
langalexandra998@gmail.com <langalexandra998@gmail.com>
2020-12-07 17:32:19 +0100

    adatok hozzaadasa

--- beadando/beadando/Form1.cs
+++ beadando/beadando/Form1.cs
@@ -7,14 +7,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
+
 
 namespace beadando
 {
+    
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+            
+           
         }
     }
 }
