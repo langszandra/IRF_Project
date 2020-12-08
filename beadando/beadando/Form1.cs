107fb1b3c4e57e7110b3fc540fb2b5bd36427771
langalexandra998@gmail.com <langalexandra998@gmail.com>
2020-12-08 22:36:20 +0100

    Enum letrehozasa

--- beadando/beadando/Form1.cs
+++ beadando/beadando/Form1.cs
@@ -24,16 +24,16 @@ namespace beadando
             DataColumn dc1 = new DataColumn("Felhasznalonev");
             DataColumn dc2 = new DataColumn("Kor");
             DataColumn dc3 = new DataColumn("Nem");
-            DataColumn dc4 = new DataColumn("Vásárlás időpontja");
+            DataColumn dc4 = new DataColumn("VásárlásIdőpontja");
 
             dt.Columns.Add(dc1);
             dt.Columns.Add(dc2);
             dt.Columns.Add(dc3);
             dt.Columns.Add(dc4);
 
-            dt.Rows.Add("langszandra", "22", "Female", "2020.11.10.");
-            dt.Rows.Add("juhaszndora", "22", "Female","2020.12.01.");
-            dt.Rows.Add("muskatkristof", "24", "Male", "2020.10.12.");
+            dt.Rows.Add("langszandra", "22", "2", "2020.11.10.");
+            dt.Rows.Add("juhaszndora", "22", "2","2020.12.01.");
+            dt.Rows.Add("muskatkristof", "24", "1", "2020.10.12.");
 
             DataSet ds = new DataSet();
             ds.DataSetName = "AdressBook";
